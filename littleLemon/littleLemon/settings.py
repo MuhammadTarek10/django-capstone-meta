2ddbd137f7d25896d59b5c252904bf30ad5388c4
Muhammad Tarek <mohammadtarek000@gmail.com>
2023-02-17 20:38:05 +0200

    nothing major

--- littleLemon/littleLemon/settings.py
+++ littleLemon/littleLemon/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+#! SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-gw39ib!ncmizur4fxh+o!d_$-x77@kza&k(h-=w_lig!_*+^g!"
 
-# SECURITY WARNING: don't run with debug turned on in production!
+#! SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
